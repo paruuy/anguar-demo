49e0010b618fa310133e7a2ca628508edfbe58f0
gocd <bst_dcita_gmdo@santander.pt>
2021-01-22 17:49:46 +0000

    config files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HeaderComponent } from './components/template/header/header.component';
+import { HeaderComponent } from './components/template/header/header.component.html';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
 
